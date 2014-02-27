15c8c4ef4604f7065e6533cc3117739371e99de6
karolsojko <zoja87@gmail.com>
2014-02-27 13:24:15 +0100

    Added about page fixes #14

--- src/TheCometCult/CommunityBundle/Controller/HomeController.php
+++ src/TheCometCult/CommunityBundle/Controller/HomeController.php
@@ -34,13 +34,35 @@ class HomeController extends Controller
             ->getRepository('TheCometCultCommunityBundle:Member')
             ->findAll();
 
+        return $this->render('TheCometCultCommunityBundle:Home:index.html.twig', array(
+            'members' => $members
+        ));
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('TheCometCultCommunityBundle:Home:about.html.twig');
+    }
+
+    public function membersCounterAction()
+    {
         $membersNumber = $this
             ->get('the_comet_cult_community.member_repository')
             ->countAll();
 
-        return $this->render('TheCometCultCommunityBundle:Home:index.html.twig', array(
-            'members' => $members,
-            'membersNumber' => $membersNumber,
+        return $this->render('TheCometCultCommunityBundle:Home:membersCounter.html.twig', array(
+            'membersNumber' => $membersNumber
+        ));
+    }
+
+    public function membershipFormAction()
+    {
+        $member = new Member();
+        $form = $this->createForm(new MemberType(), $member, array(
+            'action' => $this->generateUrl('the_comet_cult_community_homepage')
+        ));
+
+        return $this->render('TheCometCultCommunityBundle:Home:membershipForm.html.twig', array(
             'form' => $form->createView()
         ));
     }
